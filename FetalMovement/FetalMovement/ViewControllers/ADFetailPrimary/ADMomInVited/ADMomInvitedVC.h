bdb40721cd03b1ab8a028afe19e0f333038e6a39
iOSAPPliulei <liulei_prerry@liulei-2.local>
2014-03-20 15:39:51 +0800

    Revert "Merge branch 'master' of https://github.com/lanyu7/AddingTools"
    This reverts commit 15cb96c8d096f398c19e91cac7b37ca7736fe998, reversing
    changes made to a0f1feb356da1e5df1e9b13a453855df211a215f.

---
+++ FetalMovement/FetalMovement/ViewControllers/ADFetailPrimary/ADMomInVited/ADMomInvitedVC.h
@@ -0,0 +1,13 @@
+//
+//  ADMomInvitedVC.h
+//  FetalMovement
+//
+//  Created by 大头滴血 on 14-3-20.
+//  Copyright (c) 2014年 wang peng. All rights reserved.
+//
+
+#import "ADBaseViewController.h"
+
+@interface ADMomInvitedVC : ADBaseViewController
+
+@end
