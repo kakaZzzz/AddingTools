c5b6bdd06ecd7e225721c29600d15a40cad54c96
iOSAPPliulei <liulei_prerry@liulei-2.local>
2014-03-20 15:08:32 +0800

    Revert a0f1feb..9a14dce
    This rolls back to commit a0f1feb356da1e5df1e9b13a453855df211a215f.

---
+++ FetalMovement/FetalMovement/CustomLibrary/ShareView/ADShareView.h
@@ -0,0 +1,16 @@
+//
+//  ADShareView.h
+//  ShareTestDemo
+//
+//  Created by 大头滴血 on 14-3-19.
+//  Copyright (c) 2014年 liulei_prerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADShareView : UIView{
+}
+
++ (void)createShareView:(UIView *)superView;
+
+@end
---
+++ FetalMovement/FetalMovement/ViewControllers/ADFetailPrimary/ADMomInVited/ExplanationView/ADExplanationView.h
@@ -0,0 +1,16 @@
+//
+//  ADExplanationView.h
+//  FetalMovement
+//
+//  Created by 大头滴血 on 14-3-20.
+//  Copyright (c) 2014年 wang peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADExplanationView : UIView{
+
+}
+
++ (void)createShareView:(UIView *)superView;
+@end
